1b0f6c6837ce7154064ab286e791da742319949e
wang5923 <62010916+wang5923@users.noreply.github.com>
2023-03-22 17:15:03 +0800

    新增H3C backup

--- templates/h3c.py
+++ templates/h3c.py
@@ -1,8 +1,21 @@
-__all__ = ['test', 'test1', 'test2', 'test3']
+import re
+
+from netmiko import BaseConnection
+
+from API.decorator import DataHandle
+
+data = DataHandle()
+
+__all__ = ['backup_config', 'test1', 'test2', 'test3']
 
 
-def test():
-    print("H3C模块")
+@data.backup_config
+def backup_config(conn: BaseConnection):
+    output = conn.send_command(command_string='display cu')
+    file_names = re.search(r'sysname\s(.*)', output).group(1)
+    file_name = f'{file_names}.txt'
+    data = output
+    return data, file_name
 
 
 def test1():
